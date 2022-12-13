8900c860942414de7e39e1ba218887b0734ba71d
Lavrrusha <hinata135@mail.ru>
2022-12-13 18:31:23 +0300

    ДЗ

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+
+Console.Write("Введите число a: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+var num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+    Console.WriteLine($"Так как {num1} > {num2}, максимальное: {num1}");
+else 
+    Console.WriteLine($"Так как {num2} > {num1}, максимальное: {num2}");
---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
+using System.Text.RegularExpressions;
+
+string pattern = @"^[0-9 ,]$";
+while(true){
+    Console.Write("Введите числа через пробел или запятую: ");
+    string[] subs = Console.ReadLine().Split(' ', ',');
+    int[] numbers = new int[subs.Count()];
+    for(int i = 0; i < subs.Count();i++){
+        if(Regex.IsMatch(subs[i], pattern))
+            numbers[i] = Convert.ToInt32(subs[i]);
+        else{
+            Console.WriteLine("Ошибка ввода:Введите только числа через пробел или запятую!");
+            continue;
+        }
+    }
+    int numMax = 0;
+    for(int i = 0; i < numbers.Count();i++){
+        if(numbers[i] > numMax)
+            numMax = numbers[i];
+    }
+    Console.WriteLine($"Максимальное число: {numMax}");
+    break;
+}
